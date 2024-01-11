36bcbc003b0108c4f19a733d7d86c83f87e93c6e
unKILLableBRIAR <wnsgud3590@naver.com>
2024-01-11 19:31:10 +0900

    무작위 배열을 오름차순으로 정리하는 코드

--- test.c
+++ test.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
 
 int main(){
-    int arr[10] = {67,34,234,654,634,34,2,4,46,77};
+    int arr[10] = {67,34,234,654,634,94,2,4,46,77};
     int cnt = 0;
     for(int i = 0; i < 10; i++){
         printf("%d ", arr[i]);
         cnt++;
     }
 
-    printf("\n%d\n",cnt);
+    printf("\n");
+
+    for(int i = 0; i < 9; i++){
+        int flag = arr[i];
+        for(int j = i + 1; j < 10; j++){
+            if(arr[j] < flag){
+                int tmp = arr[j];
+                arr[j] = flag;
+                flag = tmp;
+                arr[i] = flag;
+            }
+        }
+    }
+
+    for(int i = 0; i < 10; i++){
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
 
     return 0;
 }
